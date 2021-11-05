03593961a984a65882c0291cff98409a319ebbda
Felipe Sousa <sousa.felipe@oulook.com>
2021-11-05 01:10:21 -0300

    Firsts migrations

--- database/migrations/1636083643323_isps.ts
+++ database/migrations/1636083643323_isps.ts
@@ -1,16 +1,37 @@
 import BaseSchema from '@ioc:Adonis/Lucid/Schema'
 
+
+
 export default class Isps extends BaseSchema {
-  protected tableName = 'isps'
+    protected tableName = 'isps'
 
-  public async up () {
-    this.schema.createTable(this.tableName, (table) => {
-      table.increments('id')
-      table.timestamps(true)
-    })
-  }
 
-  public async down () {
-    this.schema.dropTable(this.tableName)
-  }
+    public async up () {
+        this.schema.createTable(this.tableName, (table) => {
+            table.increments('id')
+            table.string('cnpj', 15).unique()
+            table.string('razao', 64)
+            table.string('nome_fantasia', 64)
+            table.string('porte', 32)
+            table.string('email', 64)
+            table.string('empresa_telefone', 11)
+            table.string('logradouro', 96)
+            table.string('cep', 9)
+            table.string('municipio', 96)
+            table.string('uf', 2)
+            table.string('titular', 96)
+            table.string('titular_contato', 11)
+            table.string('class_responsavel', 32)
+            table.enum('status', ['P', 'A', 'B', 'C']).defaultTo('P')
+            table.string('server_host', 32).unique()
+            table.string('server_ip', 15).unique()
+            table.string('token', 128).unique()
+            table.timestamps(true)
+        })
+    }
+
+
+    public async down () {
+        this.schema.dropTable(this.tableName)
+    }
 }
--- database/migrations/1636083649557_tokens.ts
+++ database/migrations/1636083649557_tokens.ts
@@ -1,16 +1,25 @@
 import BaseSchema from '@ioc:Adonis/Lucid/Schema'
 
+
+
 export default class Tokens extends BaseSchema {
-  protected tableName = 'tokens'
+    protected tableName = 'tokens'
 
-  public async up () {
-    this.schema.createTable(this.tableName, (table) => {
-      table.increments('id')
-      table.timestamps(true)
-    })
-  }
 
-  public async down () {
-    this.schema.dropTable(this.tableName)
-  }
+    public async up () {
+        this.schema.createTable(this.tableName, (table) => {
+            table.increments('id')
+            table.bigInteger('provedor')
+            table.double('valor', 11, 2)
+            table.date('validade')
+            table.enum('tipo_vencimento', ['M', 'T', 'S', 'A']).defaultTo('S')
+            table.date('data_vencimento')
+            table.string('token', 128).unique()
+            table.timestamps(true)
+        })
+    }
+
+    public async down () {
+        this.schema.dropTable(this.tableName)
+    }
 }
